29916dd0a31ee82b6b1f97b56ca962b27b2cd4a1
Michael Howitz <mh@gocept.com>
2015-05-22 09:01:41 +0200

    Add label and metadata display.

--- src/icemac/ab/calexport/masterdata/export.py
+++ src/icemac/ab/calexport/masterdata/export.py
@@ -1,9 +1,14 @@
+from icemac.addressbook.i18n import _
+import icemac.ab.calendar.masterdata.calendar
 import icemac.ab.calexport.interfaces
 import icemac.addressbook.browser.base
+import icemac.addressbook.browser.metadata
 
 
 class Masterdata(icemac.addressbook.browser.base.GroupEditForm):
     """Edit the calendar export settings."""
 
+    label = _(u'Configure the calendar html export')
+    groups = (icemac.ab.calendar.masterdata.calendar.ModifierMetadataGroup,)
     interface = icemac.ab.calexport.interfaces.IExportMasterdata
     next_url = 'parent'
