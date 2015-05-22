b89ab3d43bb41bd11ed0745ebe06b7d14269779f
Michael Howitz <mh@gocept.com>
2015-05-22 09:00:42 +0200

    Remove not used code.

--- src/icemac/ab/calexport/masterdata/export.py
+++ src/icemac/ab/calexport/masterdata/export.py
@@ -7,15 +7,3 @@ class Masterdata(icemac.addressbook.browser.base.GroupEditForm):
 
     interface = icemac.ab.calexport.interfaces.IExportMasterdata
     next_url = 'parent'
-
-
-# class AnnotationField(icemac.addressbook.browser.datamanager.AnnotationField,
-#                       grok.MultiAdapter):
-
-#     """Special AnnotationField for calendar."""
-
-#     grok.adapts(icemac.ab.calendar.interfaces.ICalendar,
-#                 zope.schema.interfaces.IField)
-
-#     no_security_proxy = (
-#         icemac.ab.calendar.interfaces.ICalendarDisplaySettings,)
