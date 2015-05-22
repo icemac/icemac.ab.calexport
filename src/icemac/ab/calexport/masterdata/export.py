5c5100875e17d8be91904cfbb9259a307311080d
Michael Howitz <mh@gocept.com>
2015-05-22 12:59:14 +0200

    Move adapter to the view which uses it.

--- src/icemac/ab/calexport/masterdata/export.py
+++ src/icemac/ab/calexport/masterdata/export.py
@@ -1,8 +1,11 @@
 from icemac.addressbook.i18n import _
+import grokcore.annotation as grok
+import icemac.ab.calendar.interfaces
 import icemac.ab.calendar.masterdata.calendar
 import icemac.ab.calexport.interfaces
 import icemac.addressbook.browser.base
 import icemac.addressbook.browser.metadata
+import zope.schema.fieldproperty
 
 
 class Masterdata(icemac.addressbook.browser.base.GroupEditForm):
@@ -12,3 +15,16 @@ class Masterdata(icemac.addressbook.browser.base.GroupEditForm):
     groups = (icemac.ab.calendar.masterdata.calendar.ModifierMetadataGroup,)
     interface = icemac.ab.calexport.interfaces.IExportMasterdata
     next_url = 'parent'
+
+
+class ExportMasterData(grok.Annotation):
+    """Store export master data in annotations."""
+
+    schema = icemac.ab.calexport.interfaces.IExportMasterdata
+    grok.context(icemac.ab.calendar.interfaces.ICalendar)
+    grok.implements(schema,
+                    zope.annotation.interfaces.IAttributeAnnotatable)
+    grok.provides(schema)
+
+    zope.schema.fieldproperty.createFieldProperties(
+        schema, omit=['categories'])
