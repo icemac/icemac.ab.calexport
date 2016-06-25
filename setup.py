9131cb16893f62ceee9a97c84cee378be46b8592
Michael Howitz <mh@gocept.com>
2016-06-25 11:15:01 +0200

    Use a more sane min version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     install_requires=[
         'grokcore.component >= 2.5.1.dev1',
         'icemac.ab.calendar >= 1.8.dev0',
-        'icemac.addressbook >= 2.6.0.dev0',
+        'icemac.addressbook >= 2.7.dev0',
         'setuptools',
     ],
     extras_require=dict(
